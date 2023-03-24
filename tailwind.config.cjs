79fb3343964b49417c06cbcb1d14ebf7c8fefb41
DaniK <danikordonezgarcia@gmail.com>
2023-03-24 23:10:50 +0100

    add new colors and font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,16 +4,20 @@ module.exports = {
 	theme: {
 		extend: {
 			backgroundImage: {
-				'portals-nous': "url('/img/portals-nous.jpg')"
+				'portals-nous': "url('/img/portals-nous.jpg')",
+				'tabgha-beach': "url('/img/tabgha-beach.jpeg')"
 			},
 			colors: {
 				'tg-one': '#CFF09E',
 				'tg-two': '#A8DBA8',
-				'tg-three': '#79BD9A',
-				'tg-four': '#3B8686'
+				'tg-three': '#1e2b61',
+				'tg-four': '#3B8686',
+				'tg-sand': '#faeed7',
+				'tg-navbar': '#fdf7e1',
+				'tb-postHeader': '#1e2b61'
 			},
 			fontFamily: {
-				'tg-title': ['Lobster']
+				'tg-title': ['Poiret One']
 			}
 		},
 	},
