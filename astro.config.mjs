0dad7e03f813de33b6cc696d18d6176570889c14
DaniK <danikordonezgarcia@gmail.com>
2023-05-11 14:32:01 +0200

    add mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import astroI18next from "astro-i18next";
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.tabgha.es',
-  integrations: [tailwind(), astroI18next(), sitemap()]
+  integrations: [tailwind(), astroI18next(), sitemap(), mdx()]
 });
